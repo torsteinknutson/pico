5b1e937f5b0eb17dbbd89de94eaffa43851f376b
Torstein Frogner Knutson <67240497+torsteinknutson@users.noreply.github.com>
2022-08-09 22:44:31 +0200

    Update lcd_player.py

--- lcd_player.py
+++ lcd_player.py
@@ -4,12 +4,28 @@ import utime
 while True:
         
     lcd = LCD()
-    string = "  WHEN I SEE CATS \n"
+    string = " WHEN*I*SEE*CATS\n"
     lcd.message(string)
-    utime.sleep(5)
+    utime.sleep(2.9)
     lcd.clear()
-    string = "  \n  I CLICK!  "
+    string = "  \n    I*CLICK!"
     lcd.message(string)
-    utime.sleep(1.5)
+    utime.sleep(2.99)
     lcd.clear()
+    
+    
+    string = "X"
+    
+    while True:
+    
+        if len(string) < 16:
+            string = " "+string
+            lcd.message(string)
+            utime.sleep(0.1)
+            lcd.clear()
+            
+                    
+        else:
+            break
+              
 
